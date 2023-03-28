77348142f7266de6efed9b960fa9e821b720a3b9
Evgeny Cherkashin <eugeneai@irnok.net>
2023-03-28 21:40:29 +0800

    Initial import

---
+++ djtestdb/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from djtestdb.users.models import User
+from djtestdb.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
